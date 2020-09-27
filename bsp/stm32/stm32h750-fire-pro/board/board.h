1c33a5b5121f08a423e34068882a705122f92562
kcmetercec <kcmeter.cec@gmail.com>
2020-09-27 10:17:56 +0800

    add(bsp): basic XIP feature for fire-750

--- bsp/stm32/stm32h750-fire-pro/board/board.h
+++ bsp/stm32/stm32h750-fire-pro/board/board.h
@@ -20,7 +20,7 @@
 extern "C" {
 #endif
 
-#define STM32_FLASH_START_ADRESS     ((uint32_t)0x08000000)
+#define STM32_FLASH_START_ADRESS     ((uint32_t)APPLICATION_ADDRESS)
 #define STM32_FLASH_SIZE             (128 * 1024)
 #define STM32_FLASH_END_ADDRESS      ((uint32_t)(STM32_FLASH_START_ADRESS + STM32_FLASH_SIZE))
 
